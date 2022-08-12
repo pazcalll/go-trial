4b785297035af84d5d2980ed7ad9462cf640006d
pazcalll <yazeedarifin@gmail.com>
2022-08-12 16:24:30 +0700

    bootstrap golang 2

--- views/view.go
+++ views/view.go
@@ -5,6 +5,7 @@ import "html/template"
 func NewView(layout string, files ...string) *View {
 	files = append(files,
 		"views/layouts/bootstrap.html",
+		"views/layouts/navbar.html",
 		"views/layouts/footer.html",
 	)
 	t, err := template.ParseFiles(files...)
